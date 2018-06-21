d1e0dffc3ea63da1b56695d082e42eb96abfe0e3
Paolo Barresi <paolobarresi@bbfactory.it>
2018-06-21 16:28:09 +0200

    fixed scripts

--- rb2d.py
+++ rb2d.py
@@ -4,15 +4,14 @@ from godot.bindings import *
 
 @exposed
 class rigidbody2d(RigidBody2D):
-#    def _ready(self):
-#        self.linear_velocity = Vector2(500, 0)
-#
-#    def _process(self, delta):
-#        if self.position.x < 0:
-#            self.position.x = 0
-#            self.linear_velocity = Vector2(500, 0)
-#
-#        if self.position.x > self.get_viewport().size.x:
-#            self.position.x = self.get_viewport().size.x
-#            self.linear_velocity = Vector2(-500, 0)
-    pass
+    def _ready(self):
+        self.linear_velocity = Vector2(500, 0)
+
+    def _process(self, delta):
+        if self.position.x < 0:
+            self.position.x = 0
+            self.linear_velocity = Vector2(500, 0)
+
+        if self.position.x > self.get_viewport().size.x:
+            self.position.x = self.get_viewport().size.x
+            self.linear_velocity = Vector2(-500, 0)
